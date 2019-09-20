fabbb293d04b0e37bb0c346129da8c216895743b
AlanRomo22 <54333632+AlanRomo22@users.noreply.github.com>
2019-09-19 21:19:26 -0300

    Codigo en proceso de la ventana de datos del alumno

--- Gestion de alumnos/Form3.Designer.cs
+++ Gestion de alumnos/Form3.Designer.cs
@@ -29,20 +29,20 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.cmboxalumno = new System.Windows.Forms.ComboBox();
+            this.cmboxaño = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.textBox9 = new System.Windows.Forms.TextBox();
-            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.txtnombreapellido = new System.Windows.Forms.TextBox();
+            this.txttelefono = new System.Windows.Forms.TextBox();
+            this.txtlocalidad = new System.Windows.Forms.TextBox();
+            this.txtdomicilio = new System.Windows.Forms.TextBox();
+            this.txtdni = new System.Windows.Forms.TextBox();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.txtfechanacimiento = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtsexo = new System.Windows.Forms.TextBox();
+            this.txttelfijo = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -53,20 +53,22 @@
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnseleccionar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cmboxalumno
             // 
-            this.comboBox1.FormattingEnabled = true;
-            resources.ApplyResources(this.comboBox1, "comboBox1");
-            this.comboBox1.Name = "comboBox1";
+            this.cmboxalumno.FormattingEnabled = true;
+            resources.ApplyResources(this.cmboxalumno, "cmboxalumno");
+            this.cmboxalumno.Name = "cmboxalumno";
+            this.cmboxalumno.SelectedIndexChanged += new System.EventHandler(this.Cmboxalumno_SelectedIndexChanged);
             // 
-            // comboBox2
+            // cmboxaño
             // 
-            this.comboBox2.FormattingEnabled = true;
-            resources.ApplyResources(this.comboBox2, "comboBox2");
-            this.comboBox2.Name = "comboBox2";
+            this.cmboxaño.FormattingEnabled = true;
+            resources.ApplyResources(this.cmboxaño, "cmboxaño");
+            this.cmboxaño.Name = "cmboxaño";
+            this.cmboxaño.SelectedIndexChanged += new System.EventHandler(this.Cmboxaño_SelectedIndexChanged);
             // 
             // label1
             // 
@@ -82,55 +84,55 @@
             this.label2.ForeColor = System.Drawing.Color.Transparent;
             this.label2.Name = "label2";
             // 
-            // textBox1
+            // txtnombreapellido
             // 
-            resources.ApplyResources(this.textBox1, "textBox1");
-            this.textBox1.Name = "textBox1";
+            resources.ApplyResources(this.txtnombreapellido, "txtnombreapellido");
+            this.txtnombreapellido.Name = "txtnombreapellido";
             // 
-            // textBox2
+            // txttelefono
             // 
-            resources.ApplyResources(this.textBox2, "textBox2");
-            this.textBox2.Name = "textBox2";
+            resources.ApplyResources(this.txttelefono, "txttelefono");
+            this.txttelefono.Name = "txttelefono";
             // 
-            // textBox3
+            // txtlocalidad
             // 
-            resources.ApplyResources(this.textBox3, "textBox3");
-            this.textBox3.Name = "textBox3";
+            resources.ApplyResources(this.txtlocalidad, "txtlocalidad");
+            this.txtlocalidad.Name = "txtlocalidad";
             // 
-            // textBox4
+            // txtdomicilio
             // 
-            resources.ApplyResources(this.textBox4, "textBox4");
-            this.textBox4.Name = "textBox4";
+            resources.ApplyResources(this.txtdomicilio, "txtdomicilio");
+            this.txtdomicilio.Name = "txtdomicilio";
             // 
-            // textBox5
+            // txtdni
             // 
-            resources.ApplyResources(this.textBox5, "textBox5");
-            this.textBox5.Name = "textBox5";
+            resources.ApplyResources(this.txtdni, "txtdni");
+            this.txtdni.Name = "txtdni";
             // 
-            // textBox6
+            // txtedad
             // 
-            resources.ApplyResources(this.textBox6, "textBox6");
-            this.textBox6.Name = "textBox6";
+            resources.ApplyResources(this.txtedad, "txtedad");
+            this.txtedad.Name = "txtedad";
             // 
-            // textBox7
+            // txtfechanacimiento
             // 
-            resources.ApplyResources(this.textBox7, "textBox7");
-            this.textBox7.Name = "textBox7";
+            resources.ApplyResources(this.txtfechanacimiento, "txtfechanacimiento");
+            this.txtfechanacimiento.Name = "txtfechanacimiento";
             // 
-            // textBox8
+            // txtemail
             // 
-            resources.ApplyResources(this.textBox8, "textBox8");
-            this.textBox8.Name = "textBox8";
+            resources.ApplyResources(this.txtemail, "txtemail");
+            this.txtemail.Name = "txtemail";
             // 
-            // textBox9
+            // txtsexo
             // 
-            resources.ApplyResources(this.textBox9, "textBox9");
-            this.textBox9.Name = "textBox9";
+            resources.ApplyResources(this.txtsexo, "txtsexo");
+            this.txtsexo.Name = "txtsexo";
             // 
-            // textBox10
+            // txttelfijo
             // 
-            resources.ApplyResources(this.textBox10, "textBox10");
-            this.textBox10.Name = "textBox10";
+            resources.ApplyResources(this.txttelfijo, "txttelfijo");
+            this.txttelfijo.Name = "txttelfijo";
             // 
             // label3
             // 
@@ -202,18 +204,18 @@
             this.label13.ForeColor = System.Drawing.Color.Transparent;
             this.label13.Name = "label13";
             // 
-            // button1
+            // btnseleccionar
             // 
-            resources.ApplyResources(this.button1, "button1");
-            this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnseleccionar, "btnseleccionar");
+            this.btnseleccionar.Name = "btnseleccionar";
+            this.btnseleccionar.UseVisualStyleBackColor = true;
             // 
             // Form3
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Gestion_de_alumnos.Properties.Resources.istockphoto_1007179744_612x612;
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnseleccionar);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
@@ -224,23 +226,24 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox10);
-            this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txttelfijo);
+            this.Controls.Add(this.txtsexo);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtfechanacimiento);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.txtdni);
+            this.Controls.Add(this.txtdomicilio);
+            this.Controls.Add(this.txtlocalidad);
+            this.Controls.Add(this.txttelefono);
+            this.Controls.Add(this.txtnombreapellido);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cmboxaño);
+            this.Controls.Add(this.cmboxalumno);
             this.ForeColor = System.Drawing.Color.Black;
             this.Name = "Form3";
             this.TransparencyKey = System.Drawing.Color.Snow;
+            this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -248,20 +251,20 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox cmboxalumno;
+        private System.Windows.Forms.ComboBox cmboxaño;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.TextBox textBox9;
-        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox txtnombreapellido;
+        private System.Windows.Forms.TextBox txttelefono;
+        private System.Windows.Forms.TextBox txtlocalidad;
+        private System.Windows.Forms.TextBox txtdomicilio;
+        private System.Windows.Forms.TextBox txtdni;
+        private System.Windows.Forms.TextBox txtedad;
+        private System.Windows.Forms.TextBox txtfechanacimiento;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtsexo;
+        private System.Windows.Forms.TextBox txttelfijo;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
@@ -272,6 +275,6 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnseleccionar;
     }
 }
--- Gestion de alumnos/Form3.cs
+++ Gestion de alumnos/Form3.cs
@@ -12,9 +12,188 @@ namespace Gestion_de_alumnos
 {
     public partial class Form3 : Form
     {
+        DataTable tablaalumno;
+
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            //Declaro los años de la secundaria
+            DataTable Tablaaño = new DataTable();
+            Tablaaño.Columns.Add("Id");
+            Tablaaño.Columns.Add("Año");
+
+            DataRow fila0 = Tablaaño.NewRow();
+            fila0[0] = "0";
+            fila0[1] = "Seleccionar";
+            Tablaaño.Rows.Add(fila0);
+
+            DataRow fila1 = Tablaaño.NewRow();
+            fila1[0] = "1";
+            fila1[1] = "1º año";
+            Tablaaño.Rows.Add(fila1);
+
+            DataRow fila2 = Tablaaño.NewRow();
+            fila2[0] = "2";
+            fila2[1] = "2º año";
+            Tablaaño.Rows.Add(fila2);
+
+            DataRow fila3 = Tablaaño.NewRow();
+            fila3[0] = "3";
+            fila3[1] = "3º año";
+            Tablaaño.Rows.Add(fila3);
+
+            DataRow fila4 = Tablaaño.NewRow();
+            fila4[0] = "4";
+            fila4[1] = "4º año";
+            Tablaaño.Rows.Add(fila4);
+
+            DataRow fila5 = Tablaaño.NewRow();
+            fila5[0] = "5";
+            fila5[1] = "5º año";
+            Tablaaño.Rows.Add(fila5);
+
+            DataRow fila6 = Tablaaño.NewRow();
+            fila6[0] = "5";
+            fila6[1] = "6º año";
+            Tablaaño.Rows.Add(fila6);
+
+            //Valuo que el año aparezca en el combobox y el ID es para ver el nº del año.
+            cmboxaño.DataSource = Tablaaño;
+            cmboxaño.ValueMember = "Id";
+            cmboxaño.DisplayMember = "Año";
+
+            //Adjunto a una columna los datos del alumno
+            tablaalumno = new DataTable();
+            tablaalumno.Columns.Add("Nombre y apellido");
+            tablaalumno.Columns.Add("Fecha de nacimiento");
+            tablaalumno.Columns.Add("Edad");
+            tablaalumno.Columns.Add("DNI");
+            tablaalumno.Columns.Add("Domicilio");
+            tablaalumno.Columns.Add("Localidad");
+            tablaalumno.Columns.Add("Telefono");
+            tablaalumno.Columns.Add("E-mail");
+            tablaalumno.Columns.Add("Tel. fijo");
+            tablaalumno.Columns.Add("Sexo");
+
+        }
+
+        private void Cmboxaño_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmboxaño.SelectedIndex > 0)
+            {
+                tablaalumno.Clear();
+                string Id = cmboxaño.SelectedValue.ToString();
+
+                switch (Id)
+                {
+                    case "1":
+                        //ESTE SERA PARA PRIMER AÑO Y ASI SUCESIVAMENTE CON LOS SIGUIENTES CASE
+                        //CARGAS DATOS DEL ALUMNO
+                        //CADA CASE REPRESENTA UN AÑO Y CON SUS RESPECTIVOS DATOS.
+                        //CASE 1:1AÑO, CASE2:2AÑO, ETC..
+                        DataRow fila10 = tablaalumno.NewRow();
+                        fila10[0] = "Aaron Castro Reyes";
+                        fila10[1] = "12/05/2007";
+                        fila10[2] = "12 años";
+                        fila10[3] = "47.168.123";
+                        fila10[4] = "Avenida Olivos 4109";
+                        fila10[5] = "Rio Cuarto";
+                        fila10[6] = "3548856915";
+                        fila10[7] = "aaroncr123@gmail.com";
+                        fila10[8] = "424124";
+                        fila10[9] = "Masculino";
+                        tablaalumno.Rows.Add(fila10);
+
+                        DataRow fila20 = tablaalumno.NewRow();
+                        fila20[0] = "Darío Puig Delgado";
+                        fila20[1] = "15/03/2006";
+                        fila20[2] = "13 años";
+                        fila20[3] = "46.224.998";
+                        fila20[4] = "Pte. Roca 4145";
+                        fila20[5] = "Villa Carlos Paz";
+                        fila20[6] = "351185134";
+                        fila20[7] = "---------";
+                        fila20[8] = "424124";
+                        fila20[9] = "Masculino";
+                        tablaalumno.Rows.Add(fila20);
+
+                        DataRow fila30 = tablaalumno.NewRow();
+                        fila30[0] = "Fátima Guerrero Parra";
+                        fila30[1] = "15/12/2007";
+                        fila30[2] = "12 años";
+                        fila30[3] = "47.888.123";
+                        fila30[4] = "Ruta Panamericana Km 36";
+                        fila30[5] = "Rio Cuarto";
+                        fila30[6] = "3548677625";
+                        fila30[7] = "fatimaguerra@outlook.com";
+                        fila30[8] = "421129";
+                        fila30[9] = "Femenino";
+                        tablaalumno.Rows.Add(fila30);
+
+                        cmboxalumno.DataSource = tablaalumno;
+                        cmboxalumno.DisplayMember = "Nombre y apellido";
+                        cmboxalumno.ValueMember = "DNI";
+                        break;
+
+                    case "2":
+                        DataRow fila11 = tablaalumno.NewRow();
+                        fila11[0] = "Aaron Castro Reyes";
+                        fila11[1] = "12/05/2007";
+                        fila11[2] = "12 años";
+                        fila11[3] = "47.168.123";
+                        fila11[4] = "Avenida Olivos 4109";
+                        fila11[5] = "Rio Cuarto";
+                        fila11[6] = "3548856915";
+                        fila11[7] = "aaroncr123@gmail.com";
+                        fila11[8] = "424124";
+                        fila11[9] = "Masculino";
+                        tablaalumno.Rows.Add(fila11);
+
+                        DataRow fila21 = tablaalumno.NewRow();
+                        fila21[0] = "Lucas Romero";
+                        fila21[1] = "11/01/2007";
+                        fila21[2] = "13 años";
+                        fila21[3] = "46.224.998";
+                        fila21[4] = "Pte. Roca 4145";
+                        fila21[5] = "Villa Carlos Paz";
+                        fila21[6] = "351185134";
+                        fila21[7] = "---------";
+                        fila21[8] = "424124";
+                        fila21[9] = "Masculino";
+                        tablaalumno.Rows.Add(fila21);
+
+                        DataRow fila31 = tablaalumno.NewRow();
+                        fila31[0] = "Laura Guerrero Rodriguez";
+                        fila31[1] = "17/11/2007";
+                        fila31[2] = "12 años";
+                        fila31[3] = "47.812.783";
+                        fila31[4] = "Ruta Sur Km 37";
+                        fila31[5] = "Rio Cuarto";
+                        fila31[6] = "3541657625";
+                        fila31[7] = "fatimaguerra@outlook.com";
+                        fila31[8] = "422329";
+                        fila31[9] = "Femenino";
+                        tablaalumno.Rows.Add(fila31);
+
+                        cmboxalumno.DataSource = tablaalumno;
+                        cmboxalumno.DisplayMember = "Nombre y apellido";
+                        cmboxalumno.ValueMember = "DNI";
+                        break;
+
+
+
+
+                }
+            }
+        }
+
+        private void Cmboxalumno_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
